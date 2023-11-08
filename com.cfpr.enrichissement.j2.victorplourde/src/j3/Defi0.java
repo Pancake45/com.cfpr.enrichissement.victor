f67791f6ad41a0fb0d53e99c8031606e79fcf962
Pancake45 <vicplourde06@gmail.com>
2023-11-08 11:54:21 -0500

    maj

--- com.cfpr.enrichissement.j2.victorplourde/src/j3/Defi0.java
+++ com.cfpr.enrichissement.j2.victorplourde/src/j3/Defi0.java
@@ -2,14 +2,14 @@ package j3;
 
 public class Defi0 {
 
+	public static final String Alphabet = "abcdefghijklmnopqrstuvwxyz";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		char[] tableau = new char[26];
-		String alphabet = "abcdefghijklmnopqrstuvwxyz";
-		
-		for (int i = 0; i < alphabet.length(); i++) {
-			tableau[i] = alphabet.charAt(i); 
+			
+		for (int i = 0; i < Alphabet.length(); i++) {
+			tableau[i] = Alphabet.charAt(i); 
 			afficherMessage(tableau[i]);
 		}
 
