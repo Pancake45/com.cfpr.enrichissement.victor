7af796e8f7e161d47a6ccb426517ff7f94472f53
Pancake45 <vicplourde06@gmail.com>
2023-11-08 11:43:35 -0500

    55

--- com.cfpr.enrichissement.j2.victorplourde/src/j3/Defi0.java
+++ com.cfpr.enrichissement.j2.victorplourde/src/j3/Defi0.java
@@ -5,6 +5,22 @@ public class Defi0 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-	}
+		char[] tableau = new char[26];
+		String alphabet = "abcdefghijklmnopqrstuvwxyz";
+		
+		for (int i = 0; i < alphabet.length(); i++) {
+			tableau[i] = alphabet.charAt(i); 
+			afficherMessage(tableau[i]);
+		}
 
+		
+		
+	}
+	public static void afficherMessage(char message) {
+		
+		System.out.println(message);
+		System.out.println("------------------------");
+	}
+	
+	
 }
